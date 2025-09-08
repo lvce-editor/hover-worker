aa43778c50227a5e7ed53c7df6a9fff34c4ccac9
Le Vivilet <levivilet@pm.me>
2025-09-08 22:11:36 +0200

    update

--- packages/hover-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/hover-worker/src/parts/CommandMap/CommandMap.ts
@@ -3,18 +3,8 @@ import * as WrapCommand from '../HoverStates/HoverStates.ts'
 import * as Create from '../Create/Create.ts'
 import * as Diff2 from '../Diff2/Diff2.ts'
 import * as Dispose from '../Dispose/Dispose.ts'
-import * as FocusFirst from '../EditorHoverFocusFirst/EditorHoverFocusFirst.ts'
-import * as FocusIndex from '../EditorHoverFocusIndex/EditorHoverFocusIndex.ts'
-import * as EditorHoverFocusNext from '../EditorHoverFocusNext/EditorHoverFocusNext.ts'
-import * as EditorHoverFocusPrevious from '../EditorHoverFocusPrevious/EditorHoverFocusPrevious.ts'
-import * as EditorHoverOpenDetails from '../EditorHoverOpenDetails/EditorHoverOpenDetails.ts'
-import * as SelectCurrent from '../EditorHoverSelectCurrent/EditorHoverSelectCurrent.ts'
-import * as SelectIndex from '../EditorHoverSelectIndex/EditorHoverSelectIndex.ts'
 import * as GetCommandIds from '../GetCommandIds/GetCommandIds.ts'
 import * as GetKeyBindings from '../GetKeyBindings/GetKeyBindings.ts'
-import * as HandleEditorDeleteLeft from '../HandleEditorDeleteLeft/HandleEditorDeleteLeft.ts'
-import * as HandleEditorType from '../HandleEditorType/HandleEditorType.ts'
-import * as HandleWheel from '../HandleWheel/HandleWheel.ts'
 import * as Initialize from '../Initialize/Initialize.ts'
 import * as LoadContent from '../LoadContent/LoadContent.ts'
 import * as Render2 from '../Render2/Render2.ts'
@@ -25,20 +15,10 @@ export const commandMap = {
   'Hovers.create': Create.create,
   'Hovers.diff2': Diff2.diff2,
   'Hovers.dispose': Dispose.dispose,
-  'Hovers.focusFirst': WrapCommand.wrapCommand(FocusFirst.focusFirst),
-  'Hovers.focusIndex': WrapCommand.wrapCommand(FocusIndex.focusIndex),
-  'Hovers.focusNext': WrapCommand.wrapCommand(EditorHoverFocusNext.focusNext),
-  'Hovers.focusPrevious': WrapCommand.wrapCommand(EditorHoverFocusPrevious.focusPrevious),
   'Hovers.getCommandIds': GetCommandIds.getCommandIds,
   'Hovers.getKeyBindings': GetKeyBindings.getKeyBindings,
-  'Hovers.handleEditorDeleteLeft': WrapCommand.wrapCommand(HandleEditorDeleteLeft.handleEditorDeleteLeft),
-  'Hovers.handleEditorType': WrapCommand.wrapCommand(HandleEditorType.handleEditorType),
-  'Hovers.handleWheel': WrapCommand.wrapCommand(HandleWheel.handleWheel),
   'Hovers.initialize': Initialize.initialize,
   'Hovers.loadContent': WrapCommand.wrapCommand(LoadContent.loadContent),
-  'Hovers.openDetails': WrapCommand.wrapCommand(EditorHoverOpenDetails.openDetails),
   'Hovers.render2': Render2.render2,
-  'Hovers.selectCurrent': WrapCommand.wrapCommand(SelectCurrent.selectCurrent),
-  'Hovers.selectIndex': WrapCommand.wrapCommand(SelectIndex.selectIndex),
   'Hovers.terminate': Terminate.terminate,
 }
--- packages/hover-worker/src/parts/CreateDefaultState/CreateDefaultState.ts
+++ packages/hover-worker/src/parts/CreateDefaultState/CreateDefaultState.ts
@@ -3,25 +3,15 @@ import type { HoverState } from '../HoverState/HoverState.ts'
 export const createDefaultState = (): HoverState => {
   return {
     uid: 0,
-    items: [],
-    itemHeight: 20,
-    maxHeight: 150,
-    minLineY: 0,
-    maxLineY: 0,
     focusedIndex: -1,
     x: 0,
     y: 0,
     width: 0,
     height: 0,
-    deltaY: 0,
-    finalDeltaY: 0,
     focused: false,
-    headerHeight: 0,
     leadingWord: '',
-    unfilteredItems: [],
     version: 0,
     editorUid: 0,
     editorLanguageId: '',
-    maxItems: 8,
   }
 }
