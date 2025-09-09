9b00e07c0f880c90b850e0c29cde69a68be88950
Le Vivilet <levivilet@pm.me>
2025-09-09 20:55:41 +0200

    feature: update command ids (#3)

--- packages/hover-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/hover-worker/src/parts/CommandMap/CommandMap.ts
@@ -11,14 +11,14 @@ import * as Render2 from '../Render2/Render2.ts'
 import * as Terminate from '../Terminate/Terminate.ts'
 
 export const commandMap = {
-  'Hovers.close': WrapCommand.wrapCommand(Close.close),
-  'Hovers.create': Create.create,
-  'Hovers.diff2': Diff2.diff2,
-  'Hovers.dispose': Dispose.dispose,
-  'Hovers.getCommandIds': GetCommandIds.getCommandIds,
-  'Hovers.getKeyBindings': GetKeyBindings.getKeyBindings,
-  'Hovers.initialize': Initialize.initialize,
-  'Hovers.loadContent': WrapCommand.wrapCommand(LoadContent.loadContent),
-  'Hovers.render2': Render2.render2,
-  'Hovers.terminate': Terminate.terminate,
+  'Hover.close': WrapCommand.wrapCommand(Close.close),
+  'Hover.create': Create.create,
+  'Hover.diff2': Diff2.diff2,
+  'Hover.dispose': Dispose.dispose,
+  'Hover.getCommandIds': GetCommandIds.getCommandIds,
+  'Hover.getKeyBindings': GetKeyBindings.getKeyBindings,
+  'Hover.initialize': Initialize.initialize,
+  'Hover.loadContent': WrapCommand.wrapCommand(LoadContent.loadContent),
+  'Hover.render2': Render2.render2,
+  'Hover.terminate': Terminate.terminate,
 }
