6bea08273ec2869c5cc40f044256921e97154fd4
Le Vivilet <levivilet@pm.me>
2025-09-09 21:41:11 +0200

    feature: improve hover content load

--- packages/hover-worker/src/parts/EditorWorker/EditorWorker.ts
+++ packages/hover-worker/src/parts/EditorWorker/EditorWorker.ts
@@ -1,3 +1,3 @@
 import { EditorWorker } from '@lvce-editor/rpc-registry'
 
-export const { invoke, set, invokeAndTransfer } = EditorWorker
+export const { invoke, set, invokeAndTransfer, getWordAtOffset2 } = EditorWorker
