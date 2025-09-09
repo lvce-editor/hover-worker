a7cfff4d76016c0167fcc8a7419cc59e36fe1c72
Le Vivilet <levivilet@pm.me>
2025-09-09 22:42:21 +0200

    feature: update command id

--- packages/hover-worker/src/parts/RenderFocusContext/RenderFocusContext.ts
+++ packages/hover-worker/src/parts/RenderFocusContext/RenderFocusContext.ts
@@ -1,6 +1,7 @@
+import { ViewletCommand } from '@lvce-editor/constants'
 import type { HoverState } from '../HoverState/HoverState.ts'
 import * as WhenExpression from '../WhenExpression/WhenExpression.ts'
 
 export const renderFocusContext = (oldState: HoverState, newState: HoverState): readonly any[] => {
-  return [/* method */ 'Viewlet.setFocusContext', WhenExpression.FocusEditorRename]
+  return [ViewletCommand.SetFocusContext, WhenExpression.FocusEditorRename]
 }
