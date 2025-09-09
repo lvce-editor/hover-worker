2f75291f4cef4d9e88467b68cbe5cb839fca4f0e
Le Vivilet <levivilet@pm.me>
2025-09-09 21:29:41 +0200

    feature: update loadContent

--- packages/hover-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/hover-worker/src/parts/LoadContent/LoadContent.ts
@@ -3,5 +3,6 @@ import type { HoverState } from '../HoverState/HoverState.ts'
 export const loadContent = async (state: HoverState): Promise<HoverState> => {
   return {
     ...state,
+    leadingWord: 'abc',
   }
 }
