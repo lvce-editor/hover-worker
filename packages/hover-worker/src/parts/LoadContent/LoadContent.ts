0f364ac32fe44c32fbc1abb09a628dacf693835c
Le Vivilet <levivilet@pm.me>
2025-09-09 21:42:37 +0200

    feature: improve hover content load (#21)

--- packages/hover-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/hover-worker/src/parts/LoadContent/LoadContent.ts
@@ -1,8 +1,16 @@
 import type { HoverState } from '../HoverState/HoverState.ts'
+import * as GetPositionAtCursor from '../GetPositionAtCursor/GetPositionAtCursor.ts'
+import * as GetWordAtOffset from '../GetWordAtOffset/GetWordAtOffset.ts'
 
 export const loadContent = async (state: HoverState): Promise<HoverState> => {
+  const { editorUid } = state
+  const wordAtOffset = await GetWordAtOffset.getWordAtOffset(editorUid)
+  const { x, y } = await GetPositionAtCursor.getPositionAtCursor(editorUid)
+
   return {
     ...state,
-    leadingWord: 'abc',
+    leadingWord: wordAtOffset,
+    x,
+    y,
   }
 }
