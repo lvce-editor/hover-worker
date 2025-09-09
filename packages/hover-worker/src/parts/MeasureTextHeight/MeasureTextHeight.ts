4b2e6aa999b84ab47b966576f2fac4fa9fc3a30c
Le Vivilet <levivilet@pm.me>
2025-09-09 21:07:36 +0200

    feature: add measureTextHeight

---
+++ packages/hover-worker/src/parts/MeasureTextHeight/MeasureTextHeight.ts
@@ -0,0 +1,10 @@
+export const measureTextBlockHeight = async (
+  text: string,
+  fontFamily: string,
+  fontSize: number,
+  lineHeight: number | string,
+  width: number,
+): Promise<number> => {
+  // TODO ask editor worker or renderer process
+  return 100
+}
