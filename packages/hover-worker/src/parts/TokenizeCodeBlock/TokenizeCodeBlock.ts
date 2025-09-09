ccb073b1ded4ac93905c1771bdf0aeb778d0334e
Le Vivilet <levivilet@pm.me>
2025-09-09 21:06:50 +0200

    feature: add tokenizeCodeBlock function (#5)

---
+++ packages/hover-worker/src/parts/TokenizeCodeBlock/TokenizeCodeBlock.ts
@@ -0,0 +1,4 @@
+export const tokenizeCodeBlock = async (codeBlock: string, languageId: string, tokenizerPath: string) => {
+  // TODO maybe use syntax highlighting worker for this or another worker
+  return []
+}
