ec7ba1d12ec6ee5b0a5fadedeb645f7a72f97c91
Le Vivilet <levivilet@pm.me>
2025-09-09 22:41:52 +0200

    feature: load hover info in loadContent (#26)

--- packages/hover-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/hover-worker/src/parts/LoadContent/LoadContent.ts
@@ -1,16 +1,27 @@
 import type { HoverState } from '../HoverState/HoverState.ts'
+import { getEditorHoverInfo } from '../GetHoverInfo/GetHoverInfo.ts'
 import * as GetPositionAtCursor from '../GetPositionAtCursor/GetPositionAtCursor.ts'
 import * as GetWordAtOffset from '../GetWordAtOffset/GetWordAtOffset.ts'
 
 export const loadContent = async (state: HoverState): Promise<HoverState> => {
-  const { editorUid } = state
+  const { editorUid, editorLanguageId } = state
   const wordAtOffset = await GetWordAtOffset.getWordAtOffset(editorUid)
   const { x, y } = await GetPositionAtCursor.getPositionAtCursor(editorUid)
 
+  const info = await getEditorHoverInfo(editorUid, editorLanguageId, undefined)
+  if (!info) {
+    return {
+      ...state,
+    }
+  }
+  const { documentation, lineInfos, matchingDiagnostics } = info
   return {
     ...state,
     leadingWord: wordAtOffset,
     x,
     y,
+    documentation,
+    lineInfos,
+    matchingDiagnostics,
   }
 }
