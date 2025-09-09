178df237fb2451de1dcb3fc188f31c0eb9de8486
Le Vivilet <levivilet@pm.me>
2025-09-09 21:26:06 +0200

    feature: add loadHover command (#13)
    * feature: add loadHover command
    
    * lint
    
    * lint

--- packages/hover-worker/src/parts/EditorCommand/EditorCommandPosition.ts
+++ packages/hover-worker/src/parts/EditorCommand/EditorCommandPosition.ts
@@ -35,10 +35,10 @@ export const at = (editor: any, eventX: number, eventY: number) => {
   }
 }
 
-/**
- * @deprecated this doesn't work for variable width characters (unicode/emoji).
- * Use position computation in renderer process instead
- *
+//  @deprecated this doesn't work for variable width characters (unicode/emoji).
+// Use position computation in renderer process instead
+
+/*
  * @param {object} editor
  * @param {number} rowIndex
  * @param {number} columnIndex
