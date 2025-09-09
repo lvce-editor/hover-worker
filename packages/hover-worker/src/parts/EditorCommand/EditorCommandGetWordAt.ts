2e5a7072a7e8b3891b80adbe848e3c29a34edd86
Le Vivilet <levivilet@pm.me>
2025-09-09 21:15:58 +0200

    feature: add getWordAt (#10)
    * feature: add getWordAt
    
    * lint

---
+++ packages/hover-worker/src/parts/EditorCommand/EditorCommandGetWordAt.ts
@@ -0,0 +1,12 @@
+import * as Character from '../Character/Character.ts'
+
+const RE_WORD_END = /[\w-]+$/
+
+export const getWordBefore = (line: string, columnIndex: number) => {
+  const before = line.slice(0, columnIndex)
+  const matchBefore = before.match(RE_WORD_END)
+  if (matchBefore) {
+    return matchBefore[0]
+  }
+  return Character.EmptyString
+}
