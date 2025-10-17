1822c7afe9c45635ca69e286ad0daee181351ab7
Le Vivilet <levivilet@pm.me>
2025-10-17 15:03:34 +0200

    feature: simplify port

--- packages/hover-worker/src/parts/EditorWorker/EditorWorker.ts
+++ packages/hover-worker/src/parts/EditorWorker/EditorWorker.ts
@@ -1,3 +1,3 @@
 import { EditorWorker } from '@lvce-editor/rpc-registry'
 
-export const { invoke, set, invokeAndTransfer, getWordAtOffset2, getWordBefore } = EditorWorker
+export const { invoke, set, invokeAndTransfer, getWordAtOffset2, getWordBefore, sendMessagePortToExtensionHostWorker } = EditorWorker
