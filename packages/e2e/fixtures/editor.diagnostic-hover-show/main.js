98e9478f8c7bf69b0fd6179e1ee3a5528a4338d5
Le Vivilet <levivilet@pm.me>
2025-10-17 13:43:06 +0200

    feature: update e2e test (#51)

--- packages/e2e/fixtures/editor.diagnostic-hover-show/main.js
+++ packages/e2e/fixtures/editor.diagnostic-hover-show/main.js
@@ -1,14 +1,19 @@
-const provider = {
+const diagnosticProvider = {
   languageId: 'xyz',
-  provideHover(textDocument, offset) {
-    return {
-      text: 'abc',
-      documentation: 'def',
-    }
+  provideDiagnostics(textDocument, offset) {
+    return [
+      {
+        rowIndex: 0,
+        columnIndex: 11,
+        endRowIndex: 0,
+        endColumnIndex: 22,
+        message: 'error',
+        type: 'error',
+      },
+    ]
   },
 }
 
 export const activate = () => {
-  // @ts-ignore
-  vscode.registerHoverProvider(provider)
+  vscode.registerDiagnosticProvider(diagnosticProvider)
 }
--- packages/e2e/src/editor.diagnostic-hover-show.ts
+++ packages/e2e/src/editor.diagnostic-hover-show.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'editor.diagnostic-hover-show'
 
+export const skip = 1
+
 export const test: Test = async ({ FileSystem, Main, Editor, Locator, expect, Extension }) => {
   // arrange
   const url = new URL('../fixtures/editor.diagnostic-hover-show', import.meta.url).toString()
