6f84f74b06b9386891de075cd922ad6abdf04265
Le Vivilet <levivilet@pm.me>
2025-10-17 15:03:18 +0200

    feature: interleave hover and diagnostic hover (#56)
    * feature: interleave hover and diagnostic hover
    
    * update
    
    * err

--- packages/hover-worker/src/parts/Hover/Hover.ts
+++ packages/hover-worker/src/parts/Hover/Hover.ts
@@ -1,10 +1,32 @@
 import * as Assert from '../Assert/Assert.ts'
 import * as ExtensionHostHover from '../ExtensionHostHover/ExtensionHostHover.ts'
 
-export const getHover = async (editorUid: number, editorLanguageId: string, offset: number) => {
-  Assert.number(editorUid)
-  Assert.number(offset)
-  // TODO invoke extension host worker directly
-  const hover = await ExtensionHostHover.executeHoverProvider(editorUid, editorLanguageId, offset)
-  return hover
+interface HoverSuccessResult {
+  readonly hover: any
+  readonly error: undefined
+}
+
+interface HoverErrorResult {
+  readonly hover: undefined
+  readonly error: unknown
+}
+
+type HoverResult = HoverSuccessResult | HoverErrorResult
+
+export const getHover = async (editorUid: number, editorLanguageId: string, offset: number): Promise<HoverResult> => {
+  try {
+    Assert.number(editorUid)
+    Assert.number(offset)
+    // TODO invoke extension host worker directly
+    const hover = await ExtensionHostHover.executeHoverProvider(editorUid, editorLanguageId, offset)
+    return {
+      hover,
+      error: undefined,
+    }
+  } catch (error) {
+    return {
+      hover: undefined,
+      error,
+    }
+  }
 }
