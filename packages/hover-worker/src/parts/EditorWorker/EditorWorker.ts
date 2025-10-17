5946ecae08924b45fc4fca6f58dbed3a6e3bb046
Le Vivilet <levivilet@pm.me>
2025-10-17 13:57:31 +0200

    feature: add getWordBefore (#53)

--- packages/hover-worker/src/parts/EditorWorker/EditorWorker.ts
+++ packages/hover-worker/src/parts/EditorWorker/EditorWorker.ts
@@ -1,3 +1,3 @@
 import { EditorWorker } from '@lvce-editor/rpc-registry'
 
-export const { invoke, set, invokeAndTransfer, getWordAtOffset2 } = EditorWorker
+export const { invoke, set, invokeAndTransfer, getWordAtOffset2, getWordBefore } = EditorWorker
