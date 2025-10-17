d993de905931941e22f2378dac2b456e827f83c1
Le Vivilet <levivilet@pm.me>
2025-10-17 13:32:01 +0200

    feature: add diagnostic hover e2e test (#50)

---
+++ packages/e2e/src/editor.diagnostic-hover-show.ts
@@ -0,0 +1,21 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'editor.diagnostic-hover-show'
+
+export const test: Test = async ({ FileSystem, Main, Editor, Locator, expect, Extension }) => {
+  // arrange
+  const url = new URL('../fixtures/editor.diagnostic-hover-show', import.meta.url).toString()
+  await Extension.addWebExtension(url)
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/src/test.xyz`, 'globalThis.AbortSignal.abort()')
+  await Main.openUri(`${tmpDir}/src/test.xyz`)
+  await Editor.setCursor(0, 11)
+
+  // act
+  await Editor.openHover()
+
+  // assert
+  const hover = Locator('.EditorHover')
+  await expect(hover).toBeVisible()
+  await expect(hover).toHaveText('def')
+}
