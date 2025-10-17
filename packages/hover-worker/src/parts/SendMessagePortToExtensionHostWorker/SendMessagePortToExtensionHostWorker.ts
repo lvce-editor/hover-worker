a7af5e2569edfa5a306e833a90a025f49edc79ac
Le Vivilet <levivilet@pm.me>
2025-10-17 15:04:31 +0200

    feature: simplify port (#58)
    * feature: simplify port
    
    * feature: simplify port

--- packages/hover-worker/src/parts/SendMessagePortToExtensionHostWorker/SendMessagePortToExtensionHostWorker.ts
+++ packages/hover-worker/src/parts/SendMessagePortToExtensionHostWorker/SendMessagePortToExtensionHostWorker.ts
@@ -1,13 +1,6 @@
 import * as EditorWorker from '../EditorWorker/EditorWorker.ts'
 
 export const sendMessagePortToExtensionHostWorker = async (port: MessagePort): Promise<void> => {
-  const command = 'HandleMessagePort.handleMessagePort2'
   // @ts-ignore
-  await EditorWorker.invokeAndTransfer(
-    // @ts-ignore
-    'SendMessagePortToExtensionHostWorker.sendMessagePortToExtensionHostWorker',
-    port,
-    command,
-    0, // TODO
-  )
+  await EditorWorker.sendMessagePortToExtensionHostWorker(port)
 }
