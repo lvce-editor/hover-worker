643079455f1c03caf1e6a9fd924319c33e4802ed
Le Vivilet <levivilet@pm.me>
2026-08-17 12:07:45 +0000

    fix: resolve shared process from package

--- packages/build/src/build-static.ts
+++ packages/build/src/build-static.ts
@@ -1,13 +1,8 @@
 import { cp } from 'node:fs/promises'
 import { join } from 'node:path'
-import { pathToFileURL } from 'node:url'
 import { root } from './root.ts'
 
-const sharedProcessPath = join(root, 'packages', 'server', 'node_modules', '@lvce-editor', 'shared-process', 'index.js')
-
-const sharedProcessUrl = pathToFileURL(sharedProcessPath).toString()
-
-const sharedProcess = await import(sharedProcessUrl)
+const sharedProcess = await import('@lvce-editor/shared-process')
 
 process.env.PATH_PREFIX = '/hover-worker'
 await sharedProcess.exportStatic({
